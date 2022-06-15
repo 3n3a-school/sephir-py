d89363f3ad278a6d48b5560ea8698b3da889168a
3n3a <gh@3n3a.ch>
2022-06-15 15:25:24 +0000

    use selenium to implement logging in

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-from pages.auth import Auth
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-# Get Cfid and Cftoken
-auth = Auth()
-auth.get_tokens()
-auth.login()
-auth.get_home()
+load_dotenv()
+
+class App:
+    def __init__(self):
+        self.user = {
+            "email": os.getenv("USERNAME"),
+            "password": os.getenv("PASSWORD")
+        }
+        self.url = "https://sephir.ch/ict/user/lernendenportal"
+        self.driver = webdriver.Firefox()
+    
+    def openLogin(self):
+        self.driver.get(f"{self.url}/login.cfm")
+
+    def fillLoginForm(self):
+        email = self.driver.find_element(By.NAME, "email")
+        password = self.driver.find_element(By.NAME, "passwort")
+        submitBtn = self.driver.find_element(By.CSS_SELECTOR, "input[type='submit']")
+
+        email.send_keys(self.user.get("email"))
+        password.send_keys(self.user.get("password"))
+        submitBtn.click()
+
+if __name__=="__main__":
+    main()
